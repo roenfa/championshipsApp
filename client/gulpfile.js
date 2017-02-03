b07f2d224106e1651a760dd82f01de7bc2bf8d89
Rocio Encinas <chio.ref@gmail.com>
2017-02-02 22:18:52 -0200

    Adding gulp client configuration

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+    gulp.src('*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'));
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('*.scss',['styles']);
 });
